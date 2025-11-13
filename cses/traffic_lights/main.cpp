18f252c2707320ea5d7d517e51bcdf3874db8b0e
William Jiang <w.jiang156@gmail.com>
2025-11-13 01:24:17 +0000

    different solution

--- cses/traffic_lights/main.cpp
+++ cses/traffic_lights/main.cpp
@@ -4,39 +4,27 @@ using namespace std;
 using ll = long long;
 using ui = unsigned int;
 
-map<ui, unordered_set<ui>> dists;
-map<ui, map<ui, unordered_set<ui>>::iterator> poles;
-
-void insert(ui pos, ui dist) {
-  auto [it, _] = dists.try_emplace(dist, unordered_set<ui>());
-  it->second.insert(pos);
-  poles[pos] = it;
-}
+set<int> lights;
+multiset<int> dist;
 
 void solve() {
   ui x;
   cin >> x;
   size_t n;
   cin >> n;
-  insert(x, x);
+  lights.insert(0);
+  lights.insert(x);
+  dist.insert(x);
   ui cur;
   for (size_t i = 0; i < n; ++i) {
     cin >> cur;
-    auto gt = poles.upper_bound(cur);
+    auto gt = lights.upper_bound(cur);
     auto lt = prev(gt);
-    ui left = gt == poles.begin() ? 0 : lt->first;
-    if (gt == poles.end() || left == cur) {
-      cout << prev(dists.end())->first << ' ';
-      continue;
-    }
-    ui right = gt->first;
-    gt->second->second.erase(right);
-    if (gt->second->second.empty()) {
-      dists.erase(gt->second);
-    }
-    insert(right, right - cur);
-    insert(cur, cur - left);
-    cout << prev(dists.end())->first << ' ';
+    dist.erase(dist.find(*gt - *lt));
+    dist.insert(*gt - cur);
+    dist.insert(cur - *lt);
+    lights.insert(cur);
+    cout << *prev(dist.end()) << ' ';
   }
   cout << endl;
 }
