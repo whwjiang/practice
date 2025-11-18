0a8b6d9f20312ec90763831c63ad7eb9134e1c73
William Jiang <w.jiang156@gmail.com>
2025-11-18 20:40:34 +0000

    ignore in/out files

--- usaco/teamwork/main.cpp
+++ usaco/teamwork/main.cpp
@@ -6,9 +6,25 @@ using ui = unsigned int;
 
 void solve() {
   ifstream read("teamwork.in");
-
-  int ans;
-  ofstream("teamwork.out") << ans << endl;
+  int n, k;
+  read >> n >> k;
+  vector<int> skills(n);
+  for (int i = 0; i < n; ++i) {
+    read >> skills[i];
+  }
+  vector<ll> dp(n, 0);
+  for (int i = 0; i < n; ++i) {
+    int cur = skills[i];
+    for (int j = i; j >= max(i - k + 1, 0); j--) {
+      cur = max(cur, skills[j]);
+      if (j > 0) {
+        dp[i] = max(dp[i], cur * (i - j + 1) + dp[j - 1]);
+      } else {
+        dp[i] = max(dp[i], (ll)cur * (i - j + 1));
+      }
+    }
+  }
+  ofstream("teamwork.out") << dp[n - 1] << endl;
 }
 
 int main() {
