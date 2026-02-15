e07dff1255b867c8325250cc51661065c00970ac
William Jiang <w.jiang156@gmail.com>
2026-02-15 08:44:40 -0600

    unfinished

--- codeforces/2184/g.cpp
+++ codeforces/2184/g.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
+// #define int long long
 /*
 Problem Link:
 Key Insight:
@@ -10,9 +10,99 @@ Solution:
 
 */
 
-void solve() {
+class SegmentTree {
+public:
+  SegmentTree(const vector<int> &vals) : n(vals.size()) {
+    tree.resize(4 * n);
+    build(vals, 1, 0, n - 1);
+  }
+
+  void update(int val, int idx) {
+    return update(val, idx, 1, 0, n - 1);
+  }
+
+  int query(int start, int end) {
+    return query(start, end, 1, 0, n - 1);
+  }
+
+private:
+  vector<int> tree;
   int n;
-  cin >> n;
+
+  void build(const vector<int> &vals, int at, int l, int r) {
+    if (l == r) {
+      tree[at] = vals[l];
+      return;
+    }
+    int mid = (l + r) / 2;
+    build(vals, at << 1, l, mid);
+    build(vals, at << 1 | 1, mid + 1, r);
+    tree[at] = min(tree[at << 1], tree[at << 1 | 1]);
+  }
+
+  void update(int val, int idx, int at, int l, int r) {
+    if (l == r) {
+      tree[at] = val;
+      return;
+    }
+    int mid = (l + r) / 2;
+    if (idx <= mid) {
+      update(val, idx, at << 1, l, mid);
+    } else {
+      update(val, idx, at << 1 | 1, mid + 1, r);
+    }
+    tree[at] = min(tree[at << 1], tree[at << 1 | 1]);
+  }
+
+  int query(int start, int end, int at, int l, int r) {
+    if (r < start || end < l) {
+      return INT_MAX;
+    }
+    if (start <= l && r <= end) {
+      return tree[at];
+    }
+    int mid = (l + r) / 2;
+    return min(query(start, end, at << 1, l, mid),
+               query(start, end, at << 1 | 1, mid + 1, r));
+  }
+};
+
+void solve() {
+  int n, q;
+  cin >> n >> q;
+  vector<int> vals(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> vals[i];
+  }
+
+  SegmentTree st(vals);
+  auto nasty = [&](int start, int end) {
+    int l = start, r = end;
+    while (l <= r) {
+      // cout << "at " << l << ' ' << r << endl;
+      int mid = (l + r) / 2;
+      int mn = st.query(start, mid);
+      // cout << "  mn: " << mn << ", mid: " << mid << endl;
+      if (mn < mid) {
+        r = mid - 1;
+      } else if (mn > mid) {
+        l = mid + 1;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  for (int i = 0; i < n; ++i) {
+    int type, a, b;
+    cin >> type >> a >> b;
+    if (type == 1) {
+      st.update(b, --a);
+    } else {
+      cout << nasty(--a, --b) << endl;
+    }
+  }
 }
 
 signed main() {
--- codeforces/2190/b.cpp
+++ codeforces/2190/b.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 #define int long long
 /*
-Problem Link:
+Problem Link: https://codeforces.com/problemset/problem/2190/B1
 Key Insight:
 
 Solution:
@@ -12,7 +12,31 @@ Solution:
 
 void solve() {
   int n;
-  cin >> n;
+  string s;
+  cin >> n >> s;
+  vector<int> nx(n + 1, n + 1);
+  vector<int> suf(n + 1, 0);
+  for (int i = n - 1; i >= 0; i--) {
+    if (s[i] == '(')
+      nx[i] = i;
+    else
+      nx[i] = nx[i + 1];
+    if (s[i] == '(')
+      suf[i] += 1;
+    suf[i] += suf[i + 1];
+  }
+
+  int ans = -1;
+  for (int i = 0; i < n; i++) {
+    if (s[i] == ')' and nx[i] <= n) {
+      int ig = nx[i] - i;
+      if (suf[nx[i] + 1] >= ig) {
+        ans = max(ans, n - 2 * ig);
+      }
+    }
+  }
+
+  cout << ans << '\n';
 }
 
 signed main() {
